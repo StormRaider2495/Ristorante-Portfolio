80f7e7e280e63b63f87ee68d7e9faf60a66def8a
StormRaider2495 <arunit95@gmail.com>
2017-04-03 16:53:34 +0530

    Initial Commit

---
+++ js/bsAddin.js
@@ -0,0 +1,11 @@
+var varVar = "var";
+let varLet = "let";
+document.getElementById('signIn').addEventListener('click',function(){
+  // var modal=document.getElementById('signInModal').modal();
+  $("#signInModal").modal({keyboard: true});
+});
+
+// $('#signInModal').on('show.bs.modal', function (event) {
+//   console.log('signIn');
+//
+// });
